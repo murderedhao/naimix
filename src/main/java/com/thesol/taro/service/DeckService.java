c75d6d66e6470f69fa10cef95abf7ff2cbe69305
MikeThesol <mike151186@gmail.com>
2024-11-14 23:03:16 +0300

    make todo for new methods

--- src/main/java/com/thesol/taro/service/DeckService.java
+++ src/main/java/com/thesol/taro/service/DeckService.java
@@ -4,6 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DeckService {
+     public int makeRask() {
+         int score = 0;
+         // TODO: Сделать метод для высчитывания общего балла за карты и сделать модель ответа
+         return score;
+     }
 
-
+     private int takeCard() {
+         // TODO: Придумать баллы за карты
+     }
 }
