27f55c197cd8f8889af21d97b61751db61338505
murderedhao <f1dg1@mail.ru>
2024-11-16 02:46:43 +0300

    create workerspage

--- src/components/Modal/Login.jsx
+++ src/components/Modal/Login.jsx
@@ -1,42 +1,8 @@
-import close from "../../assets/isClose.png";
+import close from '../../assets/isClose.png'
 
 const Login = ({ onOpen, onClose }) => {
-  if (!onOpen) return null;
+	if (!onOpen) return null
 
-<<<<<<< HEAD
-  return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black/50 overflow-hidden overflow-y-auto transition-opacity duration-300">
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        {/* Кнопка закрытия */}
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 bg-white text-black p-2 rounded-full"
-        >
-          <img src={close} alt="Закрыть" className="w-6 h-6" />
-        </button>
-        {/* Контент модального окна */}
-        <div className="bg-white p-4 rounded-lg shadow-lg w-80 h-auto flex justify-center items-center flex-col">
-          <h1 className="text-[#0c6565] text-center">Вход</h1>
-          <label className="pt-4">Почта</label>
-          <input
-            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-[#0c6565] focus:border-[#0c6565] transition duration-200"
-            type="email"
-          ></input>
-          <label className="pt-4">Пароль</label>
-          <input
-            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-[#0c6565] focus:border-[#0c6565] transition duration-200"
-            type="password"
-          ></input>
-          <button className="text-black p-2 text-sm font-normal uppercase border border-solid border-[#f15330] rounded-full leading-relaxed transition-colors ease-in-out duration-200 hover:bg-black hover:text-white hover:border-black mt-4">
-            {/* 1 */}
-            Войти
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-=======
 	return (
 		<div className='fixed top-0 left-0 w-full h-full bg-black/50 overflow-hidden overflow-y-auto transition-opacity duration-300'>
 			<div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
@@ -61,6 +27,7 @@ const Login = ({ onOpen, onClose }) => {
 						type='password'
 					></input>
 					<button className='text-black p-2 text-sm font-normal uppercase border border-solid border-[#f15330] rounded-full leading-relaxed transition-colors ease-in-out duration-200 hover:bg-black hover:text-white hover:border-black mt-4'>
+						{/* 1 */}
 						Войти
 					</button>
 				</div>
@@ -68,6 +35,5 @@ const Login = ({ onOpen, onClose }) => {
 		</div>
 	)
 }
->>>>>>> d48ab0962ac814338c8ec26af017de1e613a19c2
 
-export default Login;
+export default Login
--- src/components/Workers/Workers.jsx
+++ src/components/Workers/Workers.jsx
@@ -1,9 +1,15 @@
-const Workers = () => {
+const WorkersPage = () => {
 	return (
 		<>
-			<div>3463645645654</div>
+			<div className='flex items-center justify-between ml-[175px] mr-[175px] mt-11'>
+				<h1 className='text-2xl'>Ваш персонал</h1>
+				<button className='bg-[#0c6565] text-white px-4 py-2 rounded-full hover:bg-[#094949] transition'>
+					Добавить сотрудника
+				</button>
+			</div>
+			<div className='relative mt-[30px] mx-[150px] border'></div>
 		</>
 	)
 }
 
-export default Workers
+export default WorkersPage
