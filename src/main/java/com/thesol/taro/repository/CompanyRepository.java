782ccd6c2847e3ee48765411874979c28967a847
MikeThesol <mike151186@gmail.com>
2024-11-15 23:49:46 +0300

    it's work!! o fuck

---
+++ src/main/java/com/thesol/taro/repository/CompanyRepository.java
@@ -0,0 +1,2 @@
+package com.thesol.taro.repository;public interface CompanyRepository {
+}
--- src/main/java/com/thesol/taro/service/HRService.java
+++
@@ -1,8 +0,0 @@
-package com.thesol.taro.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HRService {
-
-}
