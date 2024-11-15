09c8f219efcb5bf7b1051787047ef561263a6964
MikeThesol <mike151186@gmail.com>
2024-11-15 11:03:54 +0300

    make method for check necessary qualities

---
+++ src/main/java/com/thesol/taro/models/Company.java
@@ -0,0 +1,9 @@
+package com.thesol.taro.models;
+
+public class Company {
+    private Long id;
+    private String name;
+    private HR hr;
+    private String address;
+    private String phone;
+}
--- src/main/java/com/thesol/taro/models/TaroCard.java
+++ src/main/java/com/thesol/taro/models/TaroCard.java
@@ -2,6 +2,9 @@ package com.thesol.taro.models;
 
 import lombok.Getter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 public class TaroCard {
     private ArcanaType arcanaType;
@@ -20,6 +23,91 @@ public class TaroCard {
         this.suit = suit;
         this.minorArcana = minorArcana;
         this.arcanaType = ArcanaType.MINOR;
+    }
+
+    public Set<NecessaryQualities> getNecessaryQualities() {
+        Set<NecessaryQualities> qualities = new HashSet<>();
+        if (arcanaType == ArcanaType.MAJOR) {
+            switch (majorArcana) {
+                case FOOL:
+                    qualities.add(NecessaryQualities.RISK);
+                    qualities.add(NecessaryQualities.OPPORTUNITY);
+                    break;
+                case MAGICIAN:
+                    qualities.add(NecessaryQualities.CREATION);
+                    qualities.add(NecessaryQualities.INNER_WISDOM);
+                    break;
+                case HIGH_PRIESTESS:
+                    qualities.add(NecessaryQualities.INTUITION);
+                    qualities.add(NecessaryQualities.INNER_WISDOM);
+                    break;
+                case EMPRESS:
+                    qualities.add(NecessaryQualities.CREATIVE_ENERGY);
+                    qualities.add(NecessaryQualities.HARMONY_IN_ACTIONS);
+                    break;
+                case EMPEROR:
+                    qualities.add(NecessaryQualities.STABILITY);
+                    qualities.add(NecessaryQualities.DISCIPLINE);
+                    break;
+                case HIEROPHANT:
+                    qualities.add(NecessaryQualities.TRADITIONS);
+                    qualities.add(NecessaryQualities.MORAL_VALUES);
+                    break;
+                case LOVERS:
+                    qualities.add(NecessaryQualities.FIND_A_BALANCE_BETWEEN_CONFLICTING_FORCES);
+                    break;
+                case CHARIOT:
+                    qualities.add(NecessaryQualities.OVERCOMING_OBSTACLES);
+                    qualities.add(NecessaryQualities.COURAGE);
+                    break;
+                case JUSTICE:
+                    qualities.add(NecessaryQualities.JUSTICE);
+                    qualities.add(NecessaryQualities.HONESTY);
+                    break;
+                case HERMIT:
+                    qualities.add(NecessaryQualities.INTROSPECTION);
+                    break;
+                case WHEEL_OF_FORTUNE:
+                    qualities.add(NecessaryQualities.READY_FOR_NEW_CHALLENGES);
+                    break;
+                case STRENGTH:
+                    qualities.add(NecessaryQualities.COURAGE);
+                    qualities.add(NecessaryQualities.PATIENCE);
+                    break;
+                case HANGED_MAN:
+                    qualities.add(NecessaryQualities.INTROSPECTION);
+                    break;
+                case DEATH:
+                    // TODO: Придумать полезное качество
+                    break;
+                case TEMPERANCE:
+                    qualities.add(NecessaryQualities.FIND_A_BALANCE_BETWEEN_CONFLICTING_FORCES);
+                    break;
+                case DEVIL:
+                    // TODO: Придумать полезное качество
+                    break;
+                case TOWER:
+                    // TODO: Придумать полезное качество
+                    break;
+                case STAR:
+                    // TODO: Придумать полезное качество
+                    break;
+                case SUN:
+                    // TODO: Придумать полезное качество
+                    break;
+                case MOON:
+                    // TODO: Придумать полезное качество
+                    break;
+                case JUDGEMENT:
+                    // TODO: Придумать полезное качество
+                    break;
+                case WORLD:
+                    // TODO: Придумать полезное качество
+                    break;
+            }
+
+
+        }
     }
 }
 
