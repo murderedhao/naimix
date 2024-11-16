16f8baa7f3d7aa1737c63b2956b268111ea80fe8
Jujjje <74712611+Jujjje@users.noreply.github.com>
2024-11-16 20:29:26 +0300

    poxui proebali

--- src/components/Workers/Workers.jsx
+++ src/components/Workers/Workers.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
 import Addworker from "../Modal/Addworker";
+import { useQuery } from "@tanstack/react-query";
+const fetchData = async () => {
+  const response = await fetch(
+    "https://6245127ed042f081.mokky.dev/enHoroscope"
+  );
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+  return data;
+};
 const WorkersPage = () => {
   const [addActive, setAddActive] = useState(false);
+  const { isPending, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchData,
+  });
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
   return (
     <>
       <div className="flex items-center justify-between ml-[175px] mr-[175px] mt-11">
@@ -16,7 +37,25 @@ const WorkersPage = () => {
           <Addworker isOpen={addActive} onClose={() => setAddActive(false)} />
         </section>
       </div>
-      <div className="relative mt-[30px] mx-[150px] border"></div>
+
+      <div className="">
+        <div className="relative mt-[30px] mx-[150px] border"></div>
+        <div className="mt-[30px] mx-[150px] h-[200px] flex  gap-y-5 px-5 gap-x-6">
+          <div className="size-[135px] bg-[#D9D9D9]"></div>
+          <div className="">
+            <p>ФИО: Никита Рекунов Максимович</p>
+            <p>Почта: example@mail.com</p>
+            <p>Телефон: 8 800 555 35 35</p>
+          </div>
+          <div className="">
+            <p>Дата рождения: 00.00.0000</p>
+            <p>Пол: Мужской</p>
+            <button className="bg-[#0C6565] text-white p-2 rounded-lg">
+              Добавить в сравнение
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
